f2993b346454e3c79ea871204cbb17cf0f9afa26
simonp20 <parker_simon@brown.edu>
2022-03-07 17:49:50 -0500

    automatically generated commit message

--- read.py
+++ read.py
@@ -1,7 +1,18 @@
-
+import numpy as np
 
 def main():
-    print("Hello world!")
+    for i in range(18):
+        string = ""
+        if i>9:
+            string = f'000{i}'
+        else:
+            string = f'0000{i}'
+        filename = f'data/Guineapig2022_{string}.asd.sco.txt'
+        data = np.loadtxt(filename)
+        spectralon = np.loadtxt("data/spectralon.txt")
+        out = data[:,1]*spectralon
+        
+        np.savetxt(f'corrected_spectra/{string}.txt', out)
 
 if __name__ == '__main__':
     main()
